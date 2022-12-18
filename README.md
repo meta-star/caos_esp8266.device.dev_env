de38ebf4304708b7365c7411551bc97915592984
poyang31 <poyang31@yahoo.com.tw>
2022-12-18 22:26:23 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # dev_env.esp8266
 
 Development environment of caOS Device System for ESP8266 chips
+
+## How to use
+
+```shell
+docker run -it ghcr.io/startw-caos/dev_env.esp8266:main
+```
--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# dev_env.esp8266 - Development environment of caOS Device System for ESP8266 chips
-# (c) 2022 Star Inc. (https://starinc.xyz)
-# License: MIT
-
-docker build -t dev_env.esp8266 .
